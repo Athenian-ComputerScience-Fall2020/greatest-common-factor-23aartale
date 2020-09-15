5bda6b45b19a2b6318431af1c28ee02ae36f3c00
23aartale <23aartale@athenian.org>
2020-09-15 21:50:14 +0000

    updated

--- my_code.py
+++ my_code.py
@@ -1,19 +1,22 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
-def find_gcf(x,y):   # Do not change function name!
-    # User code goes here
+def find_gcf(x,y):
+    for num1 in range (x,0,-1):
+        if x % num1 == 0:
+
+        y % num1 == 0:
+            
+        
 
 
-    return gcf
+
 
 
 if __name__ == '__main__':
-    # Test your code with this first
-    # Change the argument to try different values
-    print(find_gcf(6,9))
+  
+    x = int(input("Enter a number: "))
+    y = int(input("Enter a number: "))
 
-    # After you are satisfied with your results, use input() to prompt the user for two values:
-    #x = int(input("Enter a number: "))
-    #y = int(input("Enter another number: "))
+    print(find_gcf(x,y))
 
